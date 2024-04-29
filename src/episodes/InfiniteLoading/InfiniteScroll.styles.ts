406f00a517d78d5a652d7d78a24fdd54d4784fba
Rafal <rafek131@wp.pl>
2024-04-29 15:54:34 +0200

    'Infinite Loading' - items appear on button click

---
+++ src/episodes/InfiniteLoading/InfiniteScroll.styles.ts
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+	display: grid;
+	justify-content: center;
+	grid-template-columns: 400px 400px;
+	grid-template-rows: auto;
+	grid-gap: 50px 50px;
+	min-height: 100vh;
+`;
+
+export const ItemWrapper = styled.div`
+	position: relative;
+	justify-self: center;
+	padding: 20px 0 20px 30px;
+	border: 10px solid black;
+	width: 100%;
+	height: auto;
+	min-height: 400px;
+
+	img {
+		max-height: 400px;
+		object-fit: contain;
+	}
+
+	div {
+		position: absolute;
+		left: 0;
+		bottom: 10%;
+		z-index: -1;
+		display: flex;
+		flex-direction: column;
+		gap: 4px;
+		padding: 12px 10px;
+		width: 100%;
+		background-color: #ffd121;
+		text-align: right;
+		font-weight: bold;
+	}
+`;
--- src/episodes/InfiniteLoading/InfiniteScroll.tsx
+++ src/episodes/InfiniteLoading/InfiniteScroll.tsx
@@ -1,17 +1,52 @@
-import data from './beers.json';
+import { useEffect, useState } from 'react';
 import * as paginate from 'paginatejson';
+import data from './beers.json';
+import { ItemWrapper, Wrapper } from './InfiniteScroll.styles';
+import { BeerProps, PageType, beerType, fetchBeersType } from './types';
 
-const fetchBeers = (page = 1) => {
+const fetchBeers: fetchBeersType = (page = 1) => {
 	const { items, ...pageInfo } = paginate.paginate(data, page, 6);
-	return new Promise(resolve => setTimeout(() => resolve({ items, page: pageInfo }), 2500));
+	return new Promise(resolve => setTimeout(() => resolve({ items, page: pageInfo }), 25));
+};
+
+export const Beer = ({ beer: { name, image_url: ImageUrl, abv } }: BeerProps) => {
+	return (
+		<ItemWrapper>
+			<img src={ImageUrl} alt={name} />
+			<div>
+				<p>Name: {name}</p>
+				<p>ABV: {abv}%</p>
+			</div>
+		</ItemWrapper>
+	);
 };
 
 export const InfiniteScroll = () => {
+	const [beers, setBeers] = useState<never[] | beerType[]>([]);
+	const [currentPage, setCurrentPage] = useState<null | PageType>(null);
+
+	const handleLoadMoreButton = () => {
+		fetchBeers(currentPage?.next).then(res => {
+			setBeers(prevBeers => [...prevBeers, ...res.items]);
+			setCurrentPage(res.page);
+		});
+	};
+
+	useEffect(() => {
+		fetchBeers().then(res => {
+			setBeers([...res.items]);
+			setCurrentPage(res.page);
+		});
+	}, []);
+
 	return (
 		<>
-			<div>
-				<h2>Infinite scroll 🎉</h2>
-			</div>
+			<button onClick={handleLoadMoreButton}>Load more</button>
+			<Wrapper>
+				{beers.map(beer => (
+					<Beer key={beer.id} beer={beer} />
+				))}
+			</Wrapper>
 		</>
 	);
 };
